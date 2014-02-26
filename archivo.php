b607cc95f387563644593b0ef73e5002cac63d4c
Adriana <adrianillaevr@hotmail.com>
2014-02-26 17:29:25 -0600

    repetición de datos
    esta insertando dos veces los datos al csv

--- archivo.php
+++ archivo.php
@@ -2,28 +2,21 @@
 <?php 
 
 $nombre = $_POST['nombre']; //esta linea toma el dato del formulario con el nombre de la persona que se registro
-$apellido = $_POST['apellido']; 
-$cedula = $_POST['cedula']; 
-$telefono = $_POST['telefono']; 
-$correo = $_POST['correo'];
-date_default_timezone_set("America/Costa_Rica");
-$fecha=date("dmY");
-echo $nonbre;
-echo $apellido;
-echo $cedula;
-echo $telefono;
-echo $correo;
+ $apellido = $_POST['apellido']; 
+  $cedula = $_POST['cedula']; 
+   $telefono = $_POST['telefono']; 
+    $correo = $_POST['correo'];
+     date_default_timezone_set("America/Costa_Rica");
+      $fecha=date("dmY");
+        echo $nonbre;
+        echo $apellido;
+        echo $cedula;
+        echo $telefono;
+        echo $correo;
 
 
-// Tomar los campos provenientes del Formulario 
-$nombre = $_POST['nombre']; 
-$apellido = $_POST['apellido']; 
-$cedula = $_POST['cedula']; 
-$telefono = $_POST['telefono']; 
-$correo = $_POST['correo']; 
-
-
-$datos=fopen($fecha.'.csv','a');//se crea el archivo con la fecha del servidor
+//se crea el archivo con la fecha del servidor
+$datos=fopen($fecha.'.csv','a');
 	$divisor = ";";
 	$fila= $nombre.$divisor.$apellido.$divisor.$cedula.$divisor.$telefono.$divisor.$correo."\n"; 
 	fwrite($datos, $fila);

27f8a3591d8af7bbe6c742b09a5f73a05b304b32
Adriana <adrianillaevr@hotmail.com>
2014-02-26 17:18:44 -0600

    Primera parte
    archivo csv

---
+++ archivo.php
@@ -0,0 +1,33 @@
+
+<?php 
+$nombre = $_POST['nombre']; 
+$apellido = $_POST['apellido']; 
+$cedula = $_POST['cedula']; 
+$telefono = $_POST['telefono']; 
+$correo = $_POST['correo'];
+date_default_timezone_set("America/Costa_Rica");
+$fecha=date("dmY");
+echo $nonbre;
+echo $apellido;
+echo $cedula;
+echo $telefono;
+echo $correo;
+
+
+// Tomar los campos provenientes del Formulario 
+$nombre = $_POST['nombre']; 
+$apellido = $_POST['apellido']; 
+$cedula = $_POST['cedula']; 
+$telefono = $_POST['telefono']; 
+$correo = $_POST['correo']; 
+
+
+$datos=fopen($fecha.'.csv','a');//se crea el archivo con la fecha del servidor
+	$divisor = ";";
+	$fila= $nombre.$divisor.$apellido.$divisor.$cedula.$divisor.$telefono.$divisor.$correo."\n"; 
+	fwrite($datos, $fila);
+	fclose($datos);
+
+
+
+?> 

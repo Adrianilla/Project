8cb06e8ac5f243540f86c553f58788ebb46f2331
Adriana <adrianillaevr@hotmail.com>
2014-03-12 18:05:33 -0600

    mas cambios
    cambios y más cambios por que no me carga la bd

--- tabla.php
+++ tabla.php
@@ -1,22 +1,21 @@
  <?php // creando un nuevo archivo php
 
  $valores=array();// creacion de arreglo
- date_default_timezone_set("America/Costa_Rica");//asignar hora por default
-      $fecha=date("dmY");// formato especifico de hora
+ /*date_default_timezone_set("America/Costa_Rica");//asignar hora por default
+      $fecha=date("dmY");// formato especifico de hora*/
 
  //leer cada línea del archivo usando datos delimitados por coma y recibir un rango de los valores delimitados por coma
 
-$fh = fopen("$fecha.csv","r"); //abrir el archivo
-  
-
- 
-while (($data=fgetcsv($fh,1000,","))!==FALSE) {
-		$count=0;
+$fh = fopen("12032014.csv","r"); //abrir el archivo
+   
+while (($data=fgetcsv($fh,1000,","))!=FALSE) {
+	$count=0;
+	$i =0;
 		foreach ($data as $row) {
-						$count++;
-		}
-		$datos[]=$valores;
+			$valores[$i]=$row;
+			$count++;
 	}
+}
 //Conectarse y seleccionar base de datos 
 
 $link = mysql_connect('localhost', 'root',''); 
@@ -34,9 +33,9 @@ while (($data = fgetcsv($fh)))
 
 //Crea una búsqueda MySQL usando los valores delimitados por coma.
 {
- $query = sprintf('INSERT INTO tabla (Nombre,Apellido, Cedula, Telefono,Correo,) VALUES (null, "%s", "%s")', $data[0], $data[1]);
+ $query = sprintf('INSERT INTO tabla (Nombre,Apellido, Cedula, Telefono,Correo,) VALUES ("$data[0]","$data[1]","$data[2]", "$data[3]", "$data[4]")');
 //Ejecuta la búsqueda MySQL para insertar los datos en la tabla.
- $result = mysql_query($query); if (!$result) die("Error inserting data into MySQL!"); }
+ $result = mysql_query($query); if (!$result) die("Error inserting data into MySQL!". mysql_error()); }
     // cierra el archivo
 fclose($fh);
      

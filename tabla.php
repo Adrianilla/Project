80dcdd42c6ff7b4c4b4322e4e8a09af9a6476273
Adriana <adrianillaevr@hotmail.com>
2014-03-12 19:30:30 -0600

    cambios para conectar la bd
    ya conecta e inserta a la base de datos

--- tabla.php
+++ tabla.php
@@ -8,34 +8,34 @@
 
 $fh = fopen("12032014.csv","r"); //abrir el archivo
    
-while (($data=fgetcsv($fh,1000,","))!=FALSE) {
-	$count=0;
+while (($data=fgetcsv($fh,1000,";"))!=FALSE) {
+	
 	$i =0;
-		foreach ($data as $row) {
-			$valores[$i]=$row;
-			$count++;
+	foreach ($data as $row) {
+		$valores[$i]=$row;
+		$i++;
 	}
 }
 //Conectarse y seleccionar base de datos 
 
 $link = mysql_connect('localhost', 'root',''); 
 if (!$link) { 
-die('Could not connect: ' . mysql_error()); 
+	die('Could not connect: ' . mysql_error()); 
 } 
 echo 'Connected successfully'; 
 
 $db_selected = mysql_select_db('proyecto', $link); 
 if (!$db_selected) { 
-die ('Cant use proyecto: ' . mysql_error()); 
+	die ('Cant use proyecto: ' . mysql_error()); 
 } 
 
-while (($data = fgetcsv($fh))) 
 
 //Crea una búsqueda MySQL usando los valores delimitados por coma.
-{
- $query = sprintf('INSERT INTO tabla (Nombre,Apellido, Cedula, Telefono,Correo,) VALUES ("$data[0]","$data[1]","$data[2]", "$data[3]", "$data[4]")');
+
+ $query = "INSERT INTO tabla VALUES ('$valores[0]','$valores[1]','$valores[2]','$valores[3]','$valores[4]')";
 //Ejecuta la búsqueda MySQL para insertar los datos en la tabla.
- $result = mysql_query($query); if (!$result) die("Error inserting data into MySQL!". mysql_error()); }
+
+ $result = mysql_query($query); if (!$result) die("Error inserting data into MySQL!". mysql_error()); 
     // cierra el archivo
 fclose($fh);
      
